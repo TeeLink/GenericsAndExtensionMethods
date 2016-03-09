4b9ef31ec76a66b536c90f3ac56ce27522518f55
Tee Link <tesuanseylink@gmail.com>
2016-03-08 22:07:26 -0600

    Program complete

--- Program.cs
+++ Program.cs
@@ -11,7 +11,39 @@ namespace GenericsAndExtensionMethods
         static void Main(string[] args)
         {
         
-            
+        Shop<Sedan> jetts = new Shop<Sedan>();
+        Shop<Truck> shendos = new Shop<Truck>();
+        Shop<RaceCar> boogies = new Shop<RaceCar>();
+
+        Sedan s = new Sedan();
+        Truck t = new Truck();
+        RaceCar rc = new RaceCar();
+        
+        Console.WriteLine($"Sedan oil changed: {s.OilChanged}. Sedan tires filled: {s.TiresChanged}. Sedan tank filled {s.TankFilled}.");
+        Console.WriteLine($"Truck oil changed: {t.OilChanged}. Truck tires filled: {t.TiresChanged}. Truck tank filled {t.TankFilled}.");
+        Console.WriteLine($"Race car oil changed: {rc.OilChanged}. Race car tires filled: {rc.TiresChanged}. Race car tank filled {rc.TankFilled}.");
+
+
+
+        jetts.FillUp(s);
+        jetts.ChangeOil(s);
+        jetts.ChangeTires(s);
+
+        shendos.FillUp(t);
+        shendos.ChangeOil(t);
+        shendos.ChangeTires(t);
+
+        boogies.FillUp(rc);
+        boogies.ChangeOil(rc);
+        boogies.ChangeTires(rc);
+
+        Console.WriteLine($"Sedan oil changed: {s.OilChanged}. Sedan tires filled: {s.TiresChanged}. Sedan tank filled {s.TankFilled}.");
+        Console.WriteLine($"Truck oil changed: {t.OilChanged}. Truck tires filled: {t.TiresChanged}. Truck tank filled {t.TankFilled}.");
+        Console.WriteLine($"Race car oil changed: {rc.OilChanged}. Race car tires filled: {rc.TiresChanged}. Race car tank filled {rc.TankFilled}.");
+
+
+        Console.ReadLine();
+
         }
     }
 }
