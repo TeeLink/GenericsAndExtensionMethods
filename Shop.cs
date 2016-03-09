a9305b6d7981df9d3fc60851ebbadb3fdd8c4d70
Tee Link <tesuanseylink@gmail.com>
2016-03-08 20:29:33 -0600

    shop class building

---
+++ Shop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAndExtensionMethods
+{
+    class Shop<T> where T : Vehicle
+    {
+        public void FillUp(T vehicle)
+        {
+            vehicle.TankFilled = true;
+            Console.WriteLine("Tank have been filled.");
+        }
+
+        public void ChangeOil(T vehicle)
+        {
+            vehicle.OilChanged = true;
+            Console.WriteLine("Oil has been changed.");
+        }
+    }
+
+    
+}
---
+++ Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAndExtensionMethods
+{
+    class Vehicle
+    {
+        public bool OilChanged { get; set; }
+        public bool TiresChanged { get; set; }
+        public bool TankFilled { get; set; }
+    }
+
+
+}
